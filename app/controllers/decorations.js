7c1dfdc9210ceb434602f43bb6a2336d7a4edc3a
mms-gianni <gianni.carafa@srf.ch>
2019-09-19 23:56:09 +0200

    add working decorations

---
+++ app/controllers/decorations.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const config = require('../../config.js')
+
+exports.getDecoration = function (docName) {
+  console.log("getDecoration: "+docName)
+  try {
+    return JSON.parse(fs.readFileSync(config.docPath+docName+"-decoration.json", 'utf8'))
+  } catch(e) {
+    return {}
+  }
+}
+
+exports.storeDecoration = function(data, docName) {
+  console.log("storeDecoration: "+docName)
+
+  console.log(data)
+  fs.writeFileSync(config.docPath+docName+"-decoration.json", JSON.stringify(data))
+}
--- app/events/events.js
+++ app/events/events.js
@@ -2,63 +2,101 @@
 
 const docController = require('../controllers/doc.js')
 const stepController = require('../controllers/step.js')
+const decorationsController = require('../controllers/decorations.js')
 const schema = require('../../schema.js')
 const Step = require('prosemirror-transform').Step
 
 
 var events = function(socket) {
-	var nspName = socket.nsp.name
-	console.log("Namespace: "+nspName)
+    var nspName = socket.nsp.name
+    console.log("Namespace: "+nspName)
 
-	socket.on('update', async ({ version, clientID, steps }) => {
-		var storedData = docController.getDoc(nspName)
+    socket.on('update', async ({ version, clientID, steps, cursor}) => {
+        var storedData = docController.getDoc(nspName)
 
-	    // version mismatch: the stored version is newer
-	    // so we send all steps of this version back to the user
-	    if (storedData.version !== version) {
-	      socket.emit('update', {
-	        version,
-	        steps: stepController.getSteps(version),
-	      })
-	      return
-	    }
+        // load and update decortaions for the cursor
+        /*
+        const CursorDecorations = decorationsController.getDecoration(nspName)
+        CursorDecorations[clientID] = {'position': cursor, 'clientID': clientID} 
+        decorationsController.storeDecoration(CursorDecorations, nspName)
+        */
+        const CursorDecorations = {}
 
-	    let doc = schema.nodeFromJSON(storedData.doc)
+        // version mismatch: the stored version is newer
+        // so we send all steps of this version back to the user
+        if (storedData.version !== version) {
+          socket.emit('update', {
+            version,
+            steps: stepController.getSteps(version),
+            decorations: CursorDecorations,
+            clientID: clientID
+          })
+          return
+        }
 
-	    let newSteps = steps.map(step => {
-	      const newStep = Step.fromJSON(schema, step)
-	      newStep.clientID = clientID
+        let doc = schema.nodeFromJSON(storedData.doc)
 
-	      // apply step to document
-	      let result = newStep.apply(doc)
-	      doc = result.doc
+        let newSteps = steps.map(step => {
+          const newStep = Step.fromJSON(schema, step)
+          newStep.clientID = clientID
 
-	      return newStep
-	    })
+          // apply step to document
+          let result = newStep.apply(doc)
+          doc = result.doc
 
-	    // calculating a new version number is easy
-	    const newVersion = version + newSteps.length
+          return newStep
+        })
 
-	    // store data
-	    stepController.storeSteps({ version, steps: newSteps })
-   		docController.storeDoc({ version: newVersion, doc }, nspName)
+        // calculating a new version number is easy
+        const newVersion = version + newSteps.length
 
-   		// send update to everyone (me and others)
-	    socket.nsp.emit('update', {
-	        version: newVersion,
-	        steps: stepController.getSteps(version),
-	    })
-	    
 
+        // store data
+        stepController.storeSteps({ version, steps: newSteps })
+        docController.storeDoc({ version: newVersion, doc }, nspName)
+
+        // send update to everyone (me and others)
+        socket.nsp.emit('update', {
+            version: newVersion,
+            steps: stepController.getSteps(version),
+            decorations: CursorDecorations,
+            clientID: clientID
+        })
+        
         return
-	})
+    })
 
-	socket.on('disconnect', () => {
-		console.log('main.disconnect')
-    	socket.nsp.emit('getCount', socket.server.engine.clientsCount) 
-	})
+    socket.on('disconnect', () => {
+        console.log('main.disconnect')
+        socket.nsp.emit('getCount', socket.server.engine.clientsCount) 
 
-  	socket.emit('init', docController.getDoc(nspName))
+        const CursorDecorations = decorationsController.getDecoration(nspName)
+        delete CursorDecorations[socket.id] 
+        socket.nsp.emit('cursorupdate', CursorDecorations) 
+        decorationsController.storeDecoration(CursorDecorations, nspName)
+    })
+
+    socket.on('cursorchange', async ({ clientID, cursor, focused}) => {
+        console.log('main.cursorchange')
+        const CursorDecorations = decorationsController.getDecoration(nspName)
+        CursorDecorations[clientID] = {'clientID': clientID, 'cursor': cursor, 'focused': focused} 
+        socket.nsp.emit('cursorupdate', CursorDecorations) 
+        decorationsController.storeDecoration(CursorDecorations, nspName)
+    })
+
+    socket.on('cursorremove', async ({ clientID}) => {
+        console.log('main.cursorremove'+clientID)
+        const CursorDecorations = decorationsController.getDecoration(nspName)
+        delete CursorDecorations[clientID] 
+        socket.nsp.emit('cursorupdate', CursorDecorations) 
+        decorationsController.storeDecoration(CursorDecorations, nspName)
+    })
+
+    socket.emit('init', docController.getDoc(nspName), decorationsController.getDecoration(nspName))
+
+    var CursorDecorations = decorationsController.getDecoration(nspName)
+    socket.emit('cursorupdate', CursorDecorations)
+
     socket.nsp.emit('getCount', socket.server.engine.clientsCount) 
 
 }
