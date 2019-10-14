554f6a1c21689a3599e88cbabf602812a98ebb58
mms-gianni <gianni.carafa@srf.ch>
2019-10-14 15:41:52 +0200

    add separate db on MongoDB

---
+++ .docker/mongo-init.js
@@ -0,0 +1,11 @@
+let res = [
+  db.container.drop(),
+  db.container.drop(),
+  db.container.createIndex({ myfield: 1 }, { unique: true }),
+  db.container.createIndex({ thatfield: 1 }),
+  db.container.insert({ myfield: 'hello', thatfield: 'testing' }),
+  db.container.insert({ myfield: 'hello2', thatfield: 'testing' }),
+  db.container.insert({ myfield: 'hello3', thatfield: 'testing' }),
+]
+
+printjson(res)
