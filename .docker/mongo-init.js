c81b387bf21d14f13187662a480b3b5c62f95db0
mms-gianni <gianni.carafa@srf.ch>
2019-10-15 20:31:20 +0200

    in drop is enough

--- .docker/mongo-init.js
+++ .docker/mongo-init.js
@@ -1,6 +1,5 @@
 let res = [
   db.container.drop(),
-  db.container.drop(),
   db.container.createIndex({ myfield: 1 }, { unique: true }),
   db.container.createIndex({ thatfield: 1 }),
   db.container.insert({ myfield: 'hello', thatfield: 'testing' }),
